d7ffa7c2f1c9647288cbf207780a5807572f185f
k.mitarai <65850380+nyangame@users.noreply.github.com>
2022-06-02 17:00:35 +0900

    Update ShopItem.cs

--- Assets/Scripts/UI/ShopItem.cs
+++ Assets/Scripts/UI/ShopItem.cs
@@ -1,58 +1,69 @@
-using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using UnityEngine;
 
-public class CostStringConverter
+public class ShopItem : MonoBehaviour
 {
-    static public string Convert(int number)
+    [SerializeField] UnityEngine.UI.Text _name;
+    [SerializeField] UnityEngine.UI.Text _cost;
+    [SerializeField] UnityEngine.UI.Text _num;
+
+    int _itemNum = 0;
+    UnityEngine.UI.Button _button;
+    ShopItemTable _item;
+    FactoryData _data;
+
+    public void Setup(ShopItemTable item)
     {
-        string ret = "";
-        if (number < 10000)
+        _item = item;
+        _button = GetComponent<UnityEngine.UI.Button>();
+        _button.onClick.AddListener(() =>
         {
-            while (true)
-            {
-                int tmp = number % 1000;
-                if (number < 1000)
-                {
-                    ret = tmp + ret;
-                    break;
-                }
-                else
-                {
-                    ret = string.Format(",{0, 3:000}{1}",tmp, ret);
-                    number = number / 1000;
-                }
-            }
-            return ret;
+            if (Cost() > GameManager.CookieNum) return;
+
+            GameManager.Purchase(_item, Cost());
+            UpdateItem();
+        });
+
+        UpdateItem();
+    }
+
+    int Cost()
+    {
+        return Mathf.FloorToInt(_item.Cost * (1.0f + (float)(_itemNum/10.0f)));
+    }
+
+    public void UpdateItem()
+    {
+        _name.text = _item.Name;
+
+        if (_item.Type == ItemType.Factory)
+        {
+            _itemNum = GameManager.Factory.GetLevel(_item.TargetId);
+            _num.text = _itemNum.ToString();
         }
         else
         {
-            string[] DigString = new string[4]
-            {
-                "万",
-                "億",
-                "兆",
-                "京"
-            };
-            int digIndex = 0;
-            int num = number / 10000;
-            while (true)
-            {
-                int tmp = num % 10000;
-                if (num < 10000)
-                {
-                    ret = string.Format("{0: .###}{1}", (float)number / Math.Pow(10000.0f, digIndex+1), DigString[digIndex]);
-                    break;
-                }
-                else
-                {
-                    num = num / 10000;
-                    digIndex++;
-                }
-            }
-            return ret;
+            _num.text = "";
+        }
+
+        _cost.text = string.Format("Cost:{0}", CostStringConverter.Convert(Cost()));
+    }
+
+    private void Update()
+    {
+        CheckPerchase();
+    }
+
+    void CheckPerchase()
+    {
+        if (Cost() > GameManager.CookieNum)
+        {
+            _cost.color = Color.red;
+        }
+        else
+        {
+            _cost.color = Color.black;
         }
     }
 }
